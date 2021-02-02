0333935feb248beda1a91f5bf92f0bdd127c92da
Nolan Terpstra <nbakert@gmail.com>
2021-02-02 17:43:29 -0600

    save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,17 +10,25 @@ const styles = {
         textAlign: "center"
     },
     jumbotron: {
-        backgroundImage: "linear-gradient(to bottom right, red, blue)",
+        // backgroundImage: "radial-gradient(to bottom right, red, blue)",
         boxShadow: "0px 0px 10px black",
+        marginTop: "20px"
+    },
+    container: {
+        backgroundImage: "radial-gradient(gray, black)"
     }
 }
 
 function Home() {
     return (
         <>
-            <main className={"container w-75 mx-auto shadow-lg bg-transparent vw-100 vh-80 rounded"}>
-                <div style={styles.jumbotron} className="jumbotron jumbotron-fluid rounded">
-                    <div className="container">
+            <main style={styles.container} className={"container w-75 mx-auto shadow-lg vw-100 rounded"}>
+                <div style={styles.jumbotron} className="jumbotron jumbotron-fluid rounded bg-transparent">
+                    <div className="container col mx-auto">
+                        <h1 style={styles.header} className="display-4 text-center">Hello, I'm Nolan Terpstra!</h1>
+                        <p style={styles.bio} className="lead">I am a full-stack web developer with an interest in practical problem solving and passion projects. Earning a certificate in full-stack development through Rice University by October 19th, where I acquired technical skills in online collaboration while learning multiple coding languages/tools such as: HTML, CSS, Javascript, jQuery, mySql/mongoDB, and React. Throughout the length of the course, I worked with a team of other developers to create full-stack applications with mobile responsive layouts and offer an engaging user experience. I am looking forward to applying my newly developed skills to offer creative solutions and engaging user experiences.</p>
+                    </div>
+                    <div className="container col mx-auto">
                         <h1 style={styles.header} className="display-4 text-center">Hello, I'm Nolan Terpstra!</h1>
                         <p style={styles.bio} className="lead">I am a full-stack web developer with an interest in practical problem solving and passion projects. Earning a certificate in full-stack development through Rice University by October 19th, where I acquired technical skills in online collaboration while learning multiple coding languages/tools such as: HTML, CSS, Javascript, jQuery, mySql/mongoDB, and React. Throughout the length of the course, I worked with a team of other developers to create full-stack applications with mobile responsive layouts and offer an engaging user experience. I am looking forward to applying my newly developed skills to offer creative solutions and engaging user experiences.</p>
                     </div>
